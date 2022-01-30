cb6034d161577d7f83d04b2431ba3261691dbc32
nono <nono@pixiv.com>
2022-01-30 21:46:09 +0900

    レビュー方法を追加

--- ScreamingAnimals/ScreamingAnimals/Presentation/AboutThisAppView.swift
+++ ScreamingAnimals/ScreamingAnimals/Presentation/AboutThisAppView.swift
@@ -6,32 +6,56 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct AboutThisAppView: View {
     var body: some View {
 		List {
-			HStack {
-				Text("AboutThisApp.Menu.Version")
-				Spacer()
-				Text("1.0")
+			Section {
+				HStack {
+					Text("AboutThisApp.Menu.Version")
+					Spacer()
+					Text("1.0")
+				}
+				NavigationLink(
+					destination: LicensesView(),
+					label: {
+						Text("AboutThisApp.Menu.Licenses")
+					})
+				NavigationLink(
+					destination: WebView(url: URL(string: "https://screaming-animals.glitch.me/privacy-policy.html")!)
+						.navigationTitle("AboutThisApp.Menu.PrivacyPolicy"),
+					label: {
+						Text("AboutThisApp.Menu.PrivacyPolicy")
+					})
+				NavigationLink(
+					destination: WebView(url: URL(string: "https://screaming-animals.glitch.me/terms_conditions.html")!)
+						.navigationTitle("AboutThisApp.Menu.TermsConditions"),
+					label: {
+						Text("AboutThisApp.Menu.TermsConditions")
+					})
 			}
-			NavigationLink(
-				destination: LicensesView(),
-				label: {
-					Text("AboutThisApp.Menu.Licenses")
-				})
-			NavigationLink(
-				destination: WebView(url: URL(string: "https://screaming-animals.glitch.me/privacy-policy.html")!)
-					.navigationTitle("AboutThisApp.Menu.PrivacyPolicy"),
-				label: {
-					Text("AboutThisApp.Menu.PrivacyPolicy")
-				})
-			NavigationLink(
-				destination: WebView(url: URL(string: "https://screaming-animals.glitch.me/terms_conditions.html")!)
-					.navigationTitle("AboutThisApp.Menu.TermsConditions"),
-				label: {
-					Text("AboutThisApp.Menu.TermsConditions")
-				})
+
+			Section {
+				HStack {
+					Text("AboutThisApp.Menu.SubmitAReview")
+						.onTapGesture {
+							if let window = UIApplication.shared.windows.first?.windowScene {
+								SKStoreReviewController.requestReview(in: window)
+							}
+						}
+					Spacer()
+				}
+				HStack {
+					Text("AboutThisApp.Menu.SendFeedbacks")
+						.onTapGesture {
+							if let url = URL(string: "itms-apps://itunes.apple.com/app/" + "1580740019") {
+								UIApplication.shared.open(url, options: [:], completionHandler: nil)
+							}
+						}
+					Spacer()
+				}
+			}
 		}
 		.navigationTitle(Text("AboutThisApp.Title"))
     }
