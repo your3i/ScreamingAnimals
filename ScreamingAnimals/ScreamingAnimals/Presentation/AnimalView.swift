3ffe33d5a8dd5b1b606f32911bdc08a34906cbc7
your3i <your3i.dev@gmail.com>
2024-04-09 12:30:42 +0900

    refine preview context menu

--- ScreamingAnimals/ScreamingAnimals/Presentation/AnimalView.swift
+++ ScreamingAnimals/ScreamingAnimals/Presentation/AnimalView.swift
@@ -21,40 +21,42 @@ struct AnimalView: View {
 	let cellWidth: CGFloat
 
 	init(animal: Animal, cellWidth: CGFloat) {
-		self.animal = animal
 		self.cellWidth = cellWidth
 		self.viewModel = AnimalViewModel(animal: animal)
+		self.animal = animal
 	}
 
-    var body: some View {
+	var body: some View {
 		VStack {
-			CustomPreviewContextMenu {
-				KFImage(animal.image)
-					.resizable()
-					.scaledToFill()
-					.frame(width: cellWidth - 6, height: cellWidth - 6)
-					.clipShape(Circle())
-					.shadow(color: .black, radius: 2)
-			} preview: {
-				AnimalPhotoView(animal: animal)
-			} actions: {
-				let favoriteAction = UIAction(title: NSLocalizedString("Animal.ContextMenu.Action.Favorite", comment: "")) { _ in
-					animalsData.toggleFavorite(animalID: animal.id)
+			KFImage(animal.image)
+				.resizable()
+				.scaledToFill()
+				.frame(width: cellWidth - 6, height: cellWidth - 6)
+				.clipShape(Circle())
+				.contentShape(.contextMenuPreview, Circle())
+				.shadow(color: .black, radius: 1)
+				.contextMenu {
+					Button {
+						animalsData.toggleFavorite(animalID: animal.id)
+					} label: {
+						Label(animal.isFavorite ? NSLocalizedString("Animal.ContextMenu.Action.Favorite.Remove", comment: "") : NSLocalizedString("Animal.ContextMenu.Action.Favorite", comment: ""), systemImage: animal.isFavorite ? "star.fill" : "star")
+					}
+
+					Button {
+						viewModel.playRandomSound()
+					} label: {
+						Label(NSLocalizedString("Animal.ContextMenu.Action.Play", comment: ""), systemImage: "play.circle")
+					}
+				} preview: {
+					AnimalPhotoView(animal: animal)
 				}
-				let playAction = UIAction(title: NSLocalizedString("Animal.ContextMenu.Action.Play", comment: "")) { _ in
+				.onTapGesture {
 					viewModel.playRandomSound()
-				}
-				return UIMenu(title: "", children: [favoriteAction, playAction])
-			}
-			.scaleEffect(isTapped ? 1.3 : 1.0)
-			.animation(.spring(response: 0.2, dampingFraction: 0.6), value: isTapped)
-			.onTapGesture {
-				viewModel.playRandomSound()
-				isTapped.toggle()
-				DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
 					isTapped.toggle()
+					DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+						isTapped.toggle()
+					}
 				}
-			}
 			HStack {
 				Text(animal.name)
 					.foregroundColor(Color("Text"))
@@ -75,9 +77,9 @@ struct AnimalView: View {
 }
 
 struct AnimalView_Previews: PreviewProvider {
-    static var previews: some View {
+	static var previews: some View {
 		let animalImage = URL(string: "https://raw.githubusercontent.com/your3i/ScreamingAnimals/main/docs/resources/lion_1.jpg")
 		let animal = Animal(id: "123", name: "animal name", image: animalImage, imageCredit: "credit text", sounds: [])
 		AnimalView(animal: animal, cellWidth: 92)
-    }
+	}
 }
